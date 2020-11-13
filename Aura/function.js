fcd511ca31ded03f9b40940cf65fdb1354eeec78
marlenaartis <70151664+marlenaartis@users.noreply.github.com>
2020-11-12 20:21:46 -0500

    dsda

--- Aura/function.js
+++ Aura/function.js
@@ -13,3 +13,12 @@ function changeImage() {
         document.getElementById("drag-2").src = "file:///Users/marlenaartis/Desktop/classwebsite/Class-site/Aura/aura012.jpg";
     }
 }
+$( document ).ready(function() {
+ $('#headerVideoLink').magnificPopup({
+  type:'inline',
+  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
+animateDiv('.float-g');
+    animateDiv('.float-h');
+    animateDiv('.float-i');
+});
